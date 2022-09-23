6ec65811d3bae606db69b76fdb4fa4823548a0f5
maximus3 <max2000.gym5cheb@yandex.ru>
2022-09-23 17:21:41 +0300

    :sparkles: add register router

--- app/endpoints/v1/__init__.py
+++ app/endpoints/v1/__init__.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter
 
 from .auth import api_router as auth_router
 from .ping import api_router as ping_router
+from .register import api_router as register_router
 
 
 prefix = '/v1'
@@ -11,6 +12,7 @@ router = APIRouter(
 
 router.include_router(ping_router)
 router.include_router(auth_router)
+router.include_router(register_router)
 
 
 __all__ = [
