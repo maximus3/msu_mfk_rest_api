b10f7120915430f742be19ee13267ded43733851
maximus3 <max2000.gym5cheb@yandex.ru>
2022-10-01 14:23:18 +0300

    fix lint

--- app/scheduler/__init__.py
+++ app/scheduler/__init__.py
@@ -1,7 +1,8 @@
 from typing import Any
 
-from app.scheduler.contest_register import \
-    job_info as contest_register_job_info
+from app.scheduler.contest_register import (
+    job_info as contest_register_job_info,
+)
 
 
 list_of_jobs: list[dict[str, Any]] = [
