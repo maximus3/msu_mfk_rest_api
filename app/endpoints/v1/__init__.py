ec6069d53363ff0c06c1410bd2594b2d2e72ac46
maximus3 <max2000.gym5cheb@yandex.ru>
2022-10-01 11:28:20 +0300

    add course router

--- app/endpoints/v1/__init__.py
+++ app/endpoints/v1/__init__.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 
 from .auth import api_router as auth_router
+from .course import api_router as course_router
 from .logs import api_router as logs_router
 from .ping import api_router as ping_router
 from .register import api_router as register_router
@@ -15,6 +16,7 @@ router.include_router(ping_router)
 router.include_router(auth_router)
 router.include_router(register_router)
 router.include_router(logs_router)
+router.include_router(course_router)
 
 
 __all__ = [
