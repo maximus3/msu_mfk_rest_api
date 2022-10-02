167b0121de1f55a6066ca5bf3e502f31c7923c02
maximus3 <max2000.gym5cheb@yandex.ru>
2022-10-02 11:20:07 +0300

    Refactoring

--- app/scheduler/contest_register.py
+++ app/scheduler/contest_register.py
@@ -1,11 +1,9 @@
-# pylint: disable=too-many-nested-blocks
-
 import logging
 
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.database.connection import SessionManager
-from app.database.models import Course
+from app.database.models import Contest, Course, Student
 from app.utils.contest import (
     add_student_to_contest,
     get_contests,
@@ -13,6 +11,38 @@ from app.utils.contest import (
 )
 from app.utils.course import get_all_courses
 from app.utils.student import get_students_by_course
+
+
+async def check_registration(
+    session: AsyncSession,
+    course: Course,
+    contest: Contest,
+    student: Student,
+    logger: logging.Logger,
+) -> bool:
+    if await is_student_registered_on_contest(session, student.id, contest.id):
+        return False
+    add_ok, message = await add_student_to_contest(
+        session,
+        contest,
+        student,
+        course.id,
+    )
+    if not add_ok:
+        logger.error(
+            'Student "%s" not registered on contest "%s". Reason: %s',
+            student.contest_login,
+            contest.yandex_contest_id,
+            message,
+        )
+        return False
+    await session.commit()
+    logging.info(
+        'Student "%s" added to contest "%s"',
+        student.contest_login,
+        contest.yandex_contest_id,
+    )
+    return True
 
 
 async def check_students_for_contest_registration(
@@ -24,31 +54,9 @@ async def check_students_for_contest_registration(
         logger.info('Contest: %s', contest)
         was_add = False
         for student in students:
-            if not await is_student_registered_on_contest(
-                session, student.id, contest.id
-            ):
-                was_add = True
-                add_ok, message = await add_student_to_contest(
-                    session,
-                    contest,
-                    student,
-                    course.id,
-                )
-                if not add_ok:
-                    logger.error(
-                        'Student "%s" not registered on contest "%s". '
-                        'Reason: %s',
-                        student.contest_login,
-                        contest.yandex_contest_id,
-                        message,
-                    )
-                    continue
-                await session.commit()
-                logging.info(
-                    'Student "%s" added to contest "%s"',
-                    student.contest_login,
-                    contest.yandex_contest_id,
-                )
+            was_add = was_add or await check_registration(
+                session, course, contest, student, logger
+            )
         if not was_add:
             logger.info(
                 'No students added to contest "%s"', contest.yandex_contest_id
