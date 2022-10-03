bb85c7ba22ac00cace7d86407e5dc6b4be3bd1ff
maximus3 <max2000.gym5cheb@yandex.ru>
2022-10-03 16:23:01 +0300

    known hosts

--- deploy/generate.sh
+++ deploy/generate.sh
@@ -4,3 +4,6 @@ PORT=$(cat port.txt)
 
 ssh-keygen -t rsa -b 4096 -C "$USERNAME@$HOST" -q -N "" -f id_rsa
 ssh-copy-id -i id_rsa.pub -p $PORT $USERNAME@$HOST
+ssh -p $PORT $USERNAME@$HOST "ssh-keyscan $HOST > known_hosts_to_git_ci.txt;exit"
+scp -i ~/.ssh/id_rsa -P $PORT $USERNAME@$HOST:~/known_hosts_to_git_ci.txt known_hosts_to_git_ci.txt
+ssh -p $PORT $USERNAME@$HOST "rm known_hosts_to_git_ci.txt;exit"
