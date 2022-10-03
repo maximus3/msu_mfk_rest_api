86fc9407921c81668ccec6445e87d72d6e339150
maximus3 <max2000.gym5cheb@yandex.ru>
2022-10-03 12:18:27 +0300

    request to func

--- app/utils/contest/service.py
+++ app/utils/contest/service.py
@@ -1,7 +1,7 @@
 import logging
 from uuid import UUID
 
-from httpx import AsyncClient
+from httpx import AsyncClient, Response
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.config import get_settings
@@ -10,18 +10,10 @@ from app.database.models import Contest, Student
 from .database import add_student_contest_relation
 
 
-async def add_student_to_contest(
-    session: AsyncSession,
-    contest: Contest,
-    student: Student,
-    course_id: UUID,
-) -> tuple[bool, str | None]:
-    """
-    Add student to Yandex contest.
-    """
+async def make_request_to_yandex_contest(
+    contest: Contest, student: Student
+) -> Response:
     settings = get_settings()
-    logger = logging.getLogger(__name__)
-
     async with AsyncClient() as client:
         client.headers.update(
             {
@@ -34,42 +26,56 @@ async def add_student_to_contest(
             f'{contest.yandex_contest_id}/participants?'
             f'login={student.contest_login}',
         )
-        match response.status_code:
-            case 404:
-                message = f'Contest {contest.yandex_contest_id} not found'
-                return False, message
-            case 409:
-                logger.warning(
-                    'Student "%s" already registered on contest "%s"',
-                    student.contest_login,
-                    contest.yandex_contest_id,
-                )
-            case 401:
-                message = (
-                    'Yandex API key is invalid. Please check it in .env file'
-                )
-                return False, message
-            case 403:
-                message = (
-                    f'Yandex API key does not have access to the contest '
-                    f'"{contest.yandex_contest_id}"'
-                )
-                return False, message
-            case 201:
-                logger.info(
-                    'Student "%s" successfully added to contest "%s"',
-                    student.contest_login,
-                    contest.yandex_contest_id,
-                )
-            case 200:
-                logger.warning(
-                    'Student "%s" already registered on contest "%s" (code 200)',
-                    student.contest_login,
-                    contest.yandex_contest_id,
-                )
-            case _:
-                message = f'Unknown error. Status code: {response.status_code}'
-                return False, message
+    return response
+
+
+async def add_student_to_contest(
+    session: AsyncSession,
+    contest: Contest,
+    student: Student,
+    course_id: UUID,
+) -> tuple[bool, str | None]:
+    """
+    Add student to Yandex contest.
+    """
+    logger = logging.getLogger(__name__)
+
+    response = await make_request_to_yandex_contest(contest, student)
+
+    match response.status_code:
+        case 404:
+            message = f'Contest {contest.yandex_contest_id} not found'
+            return False, message
+        case 409:
+            logger.warning(
+                'Student "%s" already registered on contest "%s"',
+                student.contest_login,
+                contest.yandex_contest_id,
+            )
+        case 401:
+            message = 'Yandex API key is invalid. Please check it in .env file'
+            return False, message
+        case 403:
+            message = (
+                f'Yandex API key does not have access to the contest '
+                f'"{contest.yandex_contest_id}"'
+            )
+            return False, message
+        case 201:
+            logger.info(
+                'Student "%s" successfully added to contest "%s"',
+                student.contest_login,
+                contest.yandex_contest_id,
+            )
+        case 200:
+            logger.warning(
+                'Student "%s" already registered on contest "%s" (code 200)',
+                student.contest_login,
+                contest.yandex_contest_id,
+            )
+        case _:
+            message = f'Unknown error. Status code: {response.status_code}'
+            return False, message
 
     await add_student_contest_relation(
         session, student.id, contest.id, course_id
