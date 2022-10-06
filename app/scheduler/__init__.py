775d1aba79f9b3d5fbadbca2ad4c9b83b2f1a026
maximus3 <max2000.gym5cheb@yandex.ru>
2022-10-07 00:08:59 +0300

    todo results

--- app/scheduler/__init__.py
+++ app/scheduler/__init__.py
@@ -1,6 +1,6 @@
 from typing import Any
 
-# from .contest_results import job_info as contest_results_job_info
+# from .contest_results import job_info as contest_results_job_info  # TODO
 from .contest_register import job_info as contest_register_job_info
 from .db_dump import job_info as dump_db_job_info
 from .ping import job_info as ping_job_info
