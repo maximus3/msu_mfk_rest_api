e07c452ae70667676293a34f3f055d416f82ac6b
maximus3 <max2000.gym5cheb@yandex.ru>
2022-10-28 17:39:45 +0300

    fix none levels

--- app/schemas/contest/create.py
+++ app/schemas/contest/create.py
@@ -9,5 +9,5 @@ class ContestCreateRequest(BaseModel):
     lecture: int
     tasks_need: int
     score_max: float
-    levels: Levels
+    levels: Levels | None = None
     is_necessary: bool = True
