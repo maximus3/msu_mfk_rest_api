8b3ce48b7ea89c241547fc6f20953679b429d888
maximus3 <max2000.gym5cheb@yandex.ru>
2022-12-20 01:19:13 +0300

    off results

--- app/scheduler/__init__.py
+++ app/scheduler/__init__.py
@@ -1,8 +1,9 @@
 from typing import Any
 
 from .contest_register import job_info as contest_register_job_info
-from .contest_results import job_info as contest_results_job_info
-from .contest_results_dump import job_info as contest_results_dump_job_info
+
+# from .contest_results import job_info as contest_results_job_info
+# from .contest_results_dump import job_info as contest_results_dump_job_info
 from .db_dump import job_info as dump_db_job_info
 from .ping import job_info as ping_job_info
 
@@ -11,8 +12,8 @@ list_of_jobs: list[dict[str, Any]] = [
     contest_register_job_info,
     dump_db_job_info,
     ping_job_info,
-    contest_results_job_info,
-    contest_results_dump_job_info,
+    # contest_results_job_info,
+    # contest_results_dump_job_info,
 ]
 
 
