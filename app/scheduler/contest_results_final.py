4d1fe9219b35c9951d909049f6a6e4e43d3c4a14
maximus3 <max2000.gym5cheb@yandex.ru>
2023-01-15 10:14:08 +0300

    best process students is ok

--- app/scheduler/contest_results_final.py
+++ app/scheduler/contest_results_final.py
@@ -170,10 +170,6 @@ async def process_student(  # pylint: disable=too-many-arguments
     session: AsyncSession | None = None,
     logger: logging.Logger | None = None,
 ) -> None:
-    if student_course.is_ok:
-        return
-    if student_course.is_ok_final:
-        return
     if session is None:
         SessionManager().refresh()
         async with SessionManager().create_async_session() as session:
@@ -198,7 +194,7 @@ async def process_student(  # pylint: disable=too-many-arguments
     )
     if (
         student_contest.is_ok
-        # and student_contest.tasks_done == contest.tasks_count
+        and student_contest.score_no_deadline == contest.score_max
     ):
         return
     if student_contest.score == contest.score_max:
