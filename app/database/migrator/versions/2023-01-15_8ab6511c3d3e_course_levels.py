3a3730b4157c663ca3bb71de80d3619787cabe4d
maximus3 <max2000.gym5cheb@yandex.ru>
2023-01-15 10:00:56 +0300

    course levels

---
+++ app/database/migrator/versions/2023-01-15_8ab6511c3d3e_course_levels.py
@@ -0,0 +1,88 @@
+"""course levels
+
+Revision ID: 8ab6511c3d3e
+Revises: 8a4a8ee1b139
+Create Date: 2023-01-15 09:57:53.774062
+
+"""
+import sqlalchemy as sa
+from alembic import op
+from sqlalchemy.dialects import postgresql
+
+
+# revision identifiers, used by Alembic.
+revision = '8ab6511c3d3e'
+down_revision = '8a4a8ee1b139'
+branch_labels = None
+depends_on = None
+
+types = [
+    sa.Enum('percent', 'absolute', name='count_method'),
+]
+
+
+def upgrade() -> None:
+    for type_ in types:
+        type_.create(op.get_bind(), checkfirst=False)
+
+    op.create_table(
+        'course_levels',
+        sa.Column(
+            'id',
+            postgresql.UUID(as_uuid=True),
+            server_default=sa.text('gen_random_uuid()'),
+            nullable=False,
+        ),
+        sa.Column(
+            'dt_created',
+            sa.TIMESTAMP(timezone=True),
+            server_default=sa.text('CURRENT_TIMESTAMP'),
+            nullable=False,
+        ),
+        sa.Column(
+            'dt_updated',
+            sa.TIMESTAMP(timezone=True),
+            server_default=sa.text('CURRENT_TIMESTAMP'),
+            nullable=False,
+        ),
+        sa.Column('course_id', postgresql.UUID(as_uuid=True), nullable=False),
+        sa.Column(
+            'ok_method',
+            sa.Enum('contests_ok', 'score_sum', name='ok_method'),
+            server_default='contests_ok',
+            nullable=False,
+        ),
+        sa.Column(
+            'count_method',
+            sa.Enum('percent', 'absolute', name='count_method'),
+            server_default='percent',
+            nullable=False,
+        ),
+        sa.Column(
+            'ok_threshold', sa.Float(), server_default='100', nullable=False
+        ),
+        sa.ForeignKeyConstraint(
+            ['course_id'],
+            ['course.id'],
+            name=op.f('fk__course_levels__course_id__course'),
+            ondelete='CASCADE',
+        ),
+        sa.PrimaryKeyConstraint('id', name=op.f('pk__course_levels')),
+        sa.UniqueConstraint('id', name=op.f('uq__course_levels__id')),
+    )
+    op.create_index(
+        op.f('ix__course_levels__course_id'),
+        'course_levels',
+        ['course_id'],
+        unique=False,
+    )
+
+
+def downgrade() -> None:
+    op.drop_index(
+        op.f('ix__course_levels__course_id'), table_name='course_levels'
+    )
+    op.drop_table('course_levels')
+
+    for type_ in types:
+        type_.drop(op.get_bind(), checkfirst=False)
--- app/database/models/course.py
+++ app/database/models/course.py
@@ -20,3 +20,28 @@ class Course(BaseModel):
     default_update_on = sa.Column(
         sa.Boolean, default=True, nullable=False, server_default='true'
     )
+
+
+class CourseLevels(BaseModel):
+    __tablename__ = 'course_levels'
+
+    course_id = sa.Column(
+        sa.ForeignKey('course.id', ondelete='CASCADE'),
+        nullable=False,
+        index=True,
+    )
+    ok_method = sa.Column(
+        sa.Enum('contests_ok', 'score_sum', name='ok_method'),
+        default='contests_ok',
+        server_default='contests_ok',
+        nullable=False,
+    )
+    count_method = sa.Column(
+        sa.Enum('percent', 'absolute', name='count_method'),
+        default='percent',
+        server_default='percent',
+        nullable=False,
+    )
+    ok_threshold = sa.Column(
+        sa.Float, default=100, server_default='100', nullable=False
+    )
