7336a9834ae2aa0c1877160c3e9c81cda20fc4a1
maximus3 <max2000.gym5cheb@yandex.ru>
2023-01-15 11:27:46 +0300

    fix get message id

--- app/bot_helper/send/send_or_edit.py
+++ app/bot_helper/send/send_or_edit.py
@@ -15,9 +15,11 @@ async def send_or_edit(message: str, message_id: str | None = None) -> None:
         except MessageToEditNotFound:
             message_id = None
     if not message_id:
-        message_id = await bot.bot.send_message(
-            chat_id=get_settings().TG_ERROR_CHAT_ID,
-            text=message,
-            disable_notification=True,
-        )
+        message_id = (
+            await bot.bot.send_message(
+                chat_id=get_settings().TG_ERROR_CHAT_ID,
+                text=message,
+                disable_notification=True,
+            )
+        ).message_id
     return message_id
