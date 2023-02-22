87951a3c44ea7c04a2b65479f9afed5d228c4101
maximus3 <max2000.gym5cheb@yandex.ru>
2023-02-22 20:07:31 +0300

    5fix migration

---
+++ app/database/migrator/versions/2023-02-22_834c25260a81_add_uq.py
@@ -0,0 +1,37 @@
+"""add uq
+
+Revision ID: 834c25260a81
+Revises: 57b362bb3ba7
+Create Date: 2023-02-22 20:04:53.463772
+
+"""
+from alembic import op
+
+
+# revision identifiers, used by Alembic.
+revision = '834c25260a81'
+down_revision = '57b362bb3ba7'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    op.create_unique_constraint(
+        op.f('uq__contest_levels__id'), 'contest_levels', ['id']
+    )
+    op.create_unique_constraint(
+        op.f('uq__student_contest_levels__id'),
+        'student_contest_levels',
+        ['id'],
+    )
+
+
+def downgrade() -> None:
+    op.drop_constraint(
+        op.f('uq__student_contest_levels__id'),
+        'student_contest_levels',
+        type_='unique',
+    )
+    op.drop_constraint(
+        op.f('uq__contest_levels__id'), 'contest_levels', type_='unique'
+    )
