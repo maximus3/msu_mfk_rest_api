d46b5369ea2287226342202b16bd6765acf5f188
maximus3 <max2000.gym5cheb@yandex.ru>
2023-02-24 13:53:09 +0300

    fix contest register logs

--- app/scheduler/contest_register.py
+++ app/scheduler/contest_register.py
@@ -4,6 +4,7 @@ import uuid
 import loguru
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from app.bot_helper import send
 from app.database.connection import SessionManager
 from app.database.models import Contest, Course, Student
 from app.schemas import scheduler as scheduler_schemas
@@ -26,17 +27,17 @@ async def register_student(
     )
     if not add_ok:
         logger.error(
-            'Student "{}" not registered on contest "{}". Reason: {}',
-            student.contest_login,
-            contest.yandex_contest_id,
+            'Student {} not registered on contest {}. Reason: {}',
+            student.id,
+            contest.id,
             message,
         )
         return False
     await session.commit()
     logger.info(
-        'Student "{}" added to contest "{}"',
-        student.contest_login,
-        contest.yandex_contest_id,
+        'Student {} added to contest {}',
+        student.id,
+        contest.id,
     )
     return True
 
@@ -44,35 +45,48 @@ async def register_student(
 async def check_students_for_contest_registration(
     session: AsyncSession,
     course: Course,
-    logger: 'loguru.Logger',
+    base_logger: 'loguru.Logger',
 ) -> None:
     contests = await get_contests(session, course.id)
-    logger.info('Course "{}" has {} contests', course.name, len(contests))
+    base_logger.info('Course {} has {} contests', course.id, len(contests))
     for contest in contests:
+        contest_logger = base_logger.bind(
+            contest={
+                'id': contest.id,
+                'yandex_contest_id': contest.yandex_contest_id,
+            }
+        )
+        contest_logger.info('Contest: {}', contest)
         no_registered_students = await get_students_by_course_with_no_contest(
             session, course.id, contest.id
         )
-        logger.info('Contest: {}', contest)
-        logger.info(
+        contest_logger.info(
             'Contest "{}" has {} no registered students',
-            contest.yandex_contest_id,
+            contest.id,
             len(no_registered_students),
         )
         count = 0
         for student in no_registered_students:
+            student_logger = contest_logger.bind(
+                student={'id': student.id, 'contest_login': student.contest_login}
+            )
             try:
                 count += await register_student(
-                    session, contest, student, logger
+                    session, contest, student, student_logger
                 )
-            except Exception as e:  # pylint: disable=broad-except
-                logger.exception(
-                    'Error while register student "{}": {}', student, e
+            except Exception as exc:  # pylint: disable=broad-except
+                student_logger.exception(
+                    'Error while register student {} on contest {}: {}', student.id, contest.id, exc
                 )
-                logger.error('{}', traceback.format_exc())
-        logger.info(
-            'Successfully added {} students to contest "{}"',
+                await send.send_traceback_message_safe(
+                    logger=student_logger,
+                    message=f'Error while register student {student.id} on contest {contest.id}: {exc}',
+                    code=traceback.format_exc(),
+                )
+        contest_logger.info(
+            'Successfully added {} students to contest {}',
             count,
-            contest.yandex_contest_id,
+            contest.id,
         )
 
 
@@ -83,9 +97,12 @@ async def job(
         SessionManager().refresh()
         async with SessionManager().create_async_session() as session:
             return await job(session=session)
-    logger = loguru.logger.bind(uuid=uuid.uuid4().hex)
+    base_logger = loguru.logger.bind(uuid=uuid.uuid4().hex)
     courses = await get_all_active_courses(session)
     for course in courses:
+        logger = base_logger.bind(
+            course={'id': course.id, 'short_name': course.short_name}
+        )
         logger.info('Course: {}', course)
         await check_students_for_contest_registration(session, course, logger)
 
