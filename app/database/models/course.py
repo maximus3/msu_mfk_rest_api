0b7024cea261701112c1f9c7b9cec969af8042f0
maximus3 <max2000.gym5cheb@yandex.ru>
2023-02-27 19:07:04 +0300

    вdelete unused field in course models (without migration)

--- app/database/models/course.py
+++ app/database/models/course.py
@@ -6,26 +6,70 @@ from .base import BaseModel
 class Course(BaseModel):
     __tablename__ = 'course'
 
-    name = sa.Column(sa.String, unique=True)
-    short_name = sa.Column(sa.String, unique=True)
-    channel_link = sa.Column(sa.String)
-    chat_link = sa.Column(sa.String)
-    lk_link = sa.Column(sa.String)
-    score_max = sa.Column(
-        sa.Float, nullable=False, server_default='0.0', default=0.0
-    )
-    contest_count = sa.Column(
-        sa.Integer, nullable=False, server_default='0', default=0
-    )
-    ok_method = sa.Column(
-        sa.Enum('contests_ok', 'score_sum', name='ok_method'),
-        default='contests_ok',
+    name = sa.Column(
+        sa.String,
+        unique=True,
         nullable=False,
+        doc='Course name.',
     )
-    ok_threshold_perc = sa.Column(sa.Integer, default=100, nullable=False)
-    default_update_on = sa.Column(
-        sa.Boolean, default=True, nullable=False, server_default='true'
+    short_name = sa.Column(
+        sa.String,
+        unique=True,
+        nullable=False,
+        doc='Course short name.',
     )
+    channel_link = sa.Column(
+        sa.String,
+        nullable=True,
+        doc='Telegram channel link.',
+    )  # TODO: rename tg_channel_link
+    chat_link = sa.Column(
+        sa.String,
+        nullable=True,
+        doc='Telegram chat link.',
+    )  # TODO: rename tg_chat_link
+    lk_link = sa.Column(
+        sa.String,
+        nullable=True,
+        doc='Link for registration on lk.msu.ru for MFK students.',
+    )
+
+    info = sa.Column(
+        sa.TEXT,
+        nullable=True,
+        doc='Course info.',
+    )
+    need_info_from_students = sa.Column(
+        sa.JSON,
+        nullable=True,
+        doc='Info that students should provide. '
+        'Format: {"data": [{'
+        '"name": *info_name*, '
+        '"help": *help_text*, '
+        '"type": *regex/choose*, '
+        '"regex": *regex if type is regex else null*, '
+        '"choose": *list of options if type is choose else null*'
+        '}]}',
+    )
+
+    is_active = sa.Column(
+        sa.Boolean,
+        nullable=False,
+        default=False,
+        doc='Course activity status.',
+    )
+    is_open = sa.Column(
+        sa.Boolean,
+        nullable=False,
+        default=False,
+        doc='Is course with open registration.',
+    )
+    code_word = sa.Column(
+        sa.String,
+        nullable=True,
+        doc='Code word for not open courses.',
+    )
+
     is_open_registration = sa.Column(
         sa.Boolean,
         default=False,
@@ -33,6 +77,24 @@ class Course(BaseModel):
         server_default='false',
         doc='Is registration on this course open',
     )
+    register_start = sa.Column(
+        sa.DateTime,
+        nullable=True,
+        doc='Start of registration.',
+    )
+    register_end = sa.Column(
+        sa.DateTime,
+        nullable=True,
+        doc='End of registration.',
+    )
+
+    score_max = sa.Column(
+        sa.Float, nullable=False, server_default='0.0', default=0.0
+    )
+    contest_count = sa.Column(
+        sa.Integer, nullable=False, server_default='0', default=0
+    )
+
     is_archive = sa.Column(
         sa.Boolean,
         default=False,
@@ -41,7 +103,7 @@ class Course(BaseModel):
         doc='Is course in archive (not show in api)',
     )
 
-    def __repr__(self):  # type: ignore
+    def __repr__(self) -> str:
         return f'<Course {self.short_name}>'
 
 
