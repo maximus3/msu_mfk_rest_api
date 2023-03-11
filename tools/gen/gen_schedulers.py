4bf6b21d712b65e0dcb4f9f35cb5b92f6c1b4b76
maximus3 <max2000.gym5cheb@yandex.ru>
2023-03-11 15:24:38 +0300

    gen schemas

--- tools/gen/gen_schedulers.py
+++ tools/gen/gen_schedulers.py
@@ -8,13 +8,12 @@ from loguru import logger
 
 import tools.load_config
 from app.config import get_settings
+from app.schemas import gen as gen_schemas
 
 
 def make_data(
     jinja2_env: jinja2.Environment, *_: tp.Any, **__: tp.Any
-) -> tuple[
-    pathlib.Path, dict[str, tuple[jinja2.Template, bool, dict[str, tp.Any]]]
-]:
+) -> tuple[pathlib.Path, dict[str, gen_schemas.DataForGen]]:
     """Generate schedulers."""
 
     settings = get_settings()
@@ -33,10 +32,18 @@ def make_data(
     logger.info('Found {} schedulers in config.', len(schedulers_config))
 
     data_for_gen = {
-        '__init__': (init_template, True, {'jobs': schedulers_config}),
-        '__main__': (main_template, True, {}),
+        '__init__': gen_schemas.DataForGen(
+            template=init_template,
+            recreate=True,
+            gen_kwargs={'jobs': schedulers_config},
+        ),
+        '__main__': gen_schemas.DataForGen(
+            template=main_template, recreate=True, gen_kwargs={}
+        ),
     }
     for job_name in schedulers_config:
-        data_for_gen[job_name] = (job_template, False, {})
+        data_for_gen[job_name] = gen_schemas.DataForGen(
+            template=job_template, recreate=False, gen_kwargs={}
+        )
 
     return dir_for_create, data_for_gen
