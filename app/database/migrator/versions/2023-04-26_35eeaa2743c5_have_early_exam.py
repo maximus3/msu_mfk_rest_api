52a9d16a8f322c4b743e20d2ddbc4dcfd3acef30
maximus3 <max2000.gym5cheb@yandex.ru>
2023-04-26 11:07:28 +0300

    course have_early_exam

---
+++ app/database/migrator/versions/2023-04-26_35eeaa2743c5_have_early_exam.py
@@ -0,0 +1,24 @@
+"""have_early_exam
+
+Revision ID: 35eeaa2743c5
+Revises: 65aff99e85db
+Create Date: 2023-04-26 11:06:40.046179
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '35eeaa2743c5'
+down_revision = '65aff99e85db'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    op.add_column('course', sa.Column('have_early_exam', sa.Boolean(), server_default='false', nullable=False))
+
+
+def downgrade() -> None:
+    op.drop_column('course', 'have_early_exam')
