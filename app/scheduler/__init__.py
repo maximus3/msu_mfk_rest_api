2e8d4b667a43e6a929e37619bbeec6bc0807fc19
maximus3 <max2000.gym5cheb@yandex.ru>
2023-04-26 15:53:15 +0300

    tmp off contest register

--- app/scheduler/__init__.py
+++ app/scheduler/__init__.py
@@ -2,8 +2,6 @@
 
 from app.schemas import scheduler as scheduler_schemas
 
-from .contest_register import job as contest_register
-from .contest_register_group import job as contest_register_group
 from .db_dump import job as db_dump
 from .ping import job as ping
 from .update_results import job as update_results
@@ -23,24 +21,6 @@ list_of_jobs: list[scheduler_schemas.JobInfo] = [
         **{'trigger': 'cron', 'hour': 3, 'config': {'send_logs': True}},
         func=db_dump,
         name='db_dump',
-    ),
-    scheduler_schemas.JobInfo(
-        **{
-            'trigger': 'interval',
-            'minutes': 10,
-            'config': {'send_logs': True},
-        },
-        func=contest_register,
-        name='contest_register',
-    ),
-    scheduler_schemas.JobInfo(
-        **{
-            'trigger': 'interval',
-            'minutes': 10,
-            'config': {'send_logs': True},
-        },
-        func=contest_register_group,
-        name='contest_register_group',
     ),
     scheduler_schemas.JobInfo(
         **{'trigger': 'interval', 'hours': 1, 'config': {'send_logs': True}},
