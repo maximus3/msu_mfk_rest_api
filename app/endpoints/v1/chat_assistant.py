a3d795aa8856f794b46abc17970dabb402305aed
maximus3 <max2000.gym5cheb@yandex.ru>
2023-10-04 14:30:41 +0300

    fix chat assistant task

--- app/endpoints/v1/chat_assistant.py
+++ app/endpoints/v1/chat_assistant.py
@@ -76,7 +76,7 @@ async def chat_assistant(
             contest_number=chat_assistant_request.contest_number,
             task_number=chat_assistant_request.task_number,
             user_query=chat_assistant_request.user_query,
-        ),
+        ).dict(),
         student_tg_id=request.headers['log-tg-id'],
         request_id=request.scope['request_id'],
     )
