e586e4d568653743e086f159f3903bb7fd353afe
maximus3 <max2000.gym5cheb@yandex.ru>
2023-10-04 14:57:33 +0300

    add limiter

--- app/endpoints/v1/chat_assistant.py
+++ app/endpoints/v1/chat_assistant.py
@@ -25,8 +25,8 @@ api_router = APIRouter(
     '',
     status_code=status.HTTP_200_OK,
 )
-# @limiter.limit('5/1day', key_func=lambda request: request.headers['log-tg-id'])
-@limiter.limit('100/5minute', key_func=lambda request: request.headers['log-tg-id'])
+@limiter.limit('10/1day', key_func=lambda request: request.headers['log-tg-id'])
+@limiter.limit('1/2minute', key_func=lambda request: request.headers['log-tg-id'])
 async def chat_assistant(
     request: requests.Request,
     chat_assistant_request: chat_assistant_schemas.ChatAssistantRequest,
