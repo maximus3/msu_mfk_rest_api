66716a5f0959fe3195fe2d36a76ee513324572da
maximus3 <max2000.gym5cheb@yandex.ru>
2023-10-30 14:02:45 +0300

    some fixes

--- db/init_master.sh
+++ db/init_master.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 set -e
 
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" -f init_master.sql
-
-cp /etc/postgresql/pg_hba.conf /var/lib/postgresql/data/pgdata/pg_hba.conf
+PGPASSWORD=$POSTGRES_PASSWORD psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" -f init_master.sql
