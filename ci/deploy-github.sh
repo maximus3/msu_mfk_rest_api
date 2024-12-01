b25f736bdaceb7faf110a662624719e169a6839b
maximus3 <max2000.gym5cheb@yandex.ru>
2024-12-01 19:33:45 +0500

    try fix ci cd deploy

--- ci/deploy-github.sh
+++ ci/deploy-github.sh
@@ -1,21 +1,16 @@
 #!/bin/bash
 echo "Environment: $CI_ENVIRONMENT_NAME"
 
-echo "GITHUB_URL=$(git remote get-url origin)" >> $GITHUB_ENV
-echo "GITHUB_REPO=${{ github.repository }}" >> $GITHUB_ENV
-
 mkdir -p ~/.ssh
-echo "${{ secrets.SSH_KEY }}" | tr -d '\r' > ~/.ssh/deploy_key
+echo "$SSH_KEY" | tr -d '\r' > ~/.ssh/deploy_key
 sudo chmod 600 ~/.ssh/deploy_key
-ssh -i ~/.ssh/deploy_key -p $SSH_PORT -o StrictHostKeyChecking=no $SSH_USERNAME@$SSH_HOST "echo "Ping" && exit"
-ssh -i ~/.ssh/deploy_key -p ${{ secrets.SSH_PORT }} -o StrictHostKeyChecking=no ${{ secrets.SSH_USERNAME }}@${{ secrets.SSH_ADDRESS }} "
-  echo "Ping" && exit"
-ssh -i ~/.ssh/deploy_key -p ${{ secrets.SSH_PORT }} -o StrictHostKeyChecking=no ${{ secrets.SSH_USERNAME }}@${{ secrets.SSH_ADDRESS }} "
-  [ -e ${{ github.event.repository.name }} ] || git clone git@github.com:$GITHUB_REPO.git; exit"
-ssh  -i ~/.ssh/deploy_key -p ${{ secrets.SSH_PORT }} -o StrictHostKeyChecking=no ${{ secrets.SSH_USERNAME }}@${{ secrets.SSH_ADDRESS }} "
-  cd ~/${{ github.event.repository.name }};
+ssh -i ~/.ssh/deploy_key -p $SSH_PORT -o StrictHostKeyChecking=no $SSH_USERNAME@$SSH_HOST "echo Ping && exit"
+ssh -i ~/.ssh/deploy_key -p $SSH_PORT -o StrictHostKeyChecking=no $SSH_USERNAME@$SSH_HOST "
+  [ -e $CODE_FOLDER ] || git clone git@github.com:$GITHUB_REPO.git $CODE_FOLDER; exit"
+ssh -i ~/.ssh/deploy_key -o StrictHostKeyChecking=no -p $SSH_PORT $SSH_USERNAME@$SSH_HOST "
+  cd $CODE_FOLDER;
   git pull;
-  echo \"${{ secrets.ENV }}\" > .env;
+  echo \"$ENV\" > .env;
   make docker-stop postgres || echo \"Postgres not run\";
   make update;
   exit"
--- ci/deploy.sh
+++ ci/deploy.sh
@@ -4,13 +4,11 @@ apt-get update && apt-get install openssh-client -y
 mkdir -p ~/.ssh
 cat $SSH_KEY_FILE | tr -d '\r' > ~/.ssh/deploy_key
 chmod 600 ~/.ssh/deploy_key
-ssh -i ~/.ssh/deploy_key -p $SSH_PORT -o StrictHostKeyChecking=no $SSH_USERNAME@$SSH_HOST "echo "Ping" && exit"
+ssh -i ~/.ssh/deploy_key -p $SSH_PORT -o StrictHostKeyChecking=no $SSH_USERNAME@$SSH_HOST "echo Ping && exit"
 ssh -i ~/.ssh/deploy_key -p $SSH_PORT -o StrictHostKeyChecking=no $SSH_USERNAME@$SSH_HOST "
-  [ -e $CODE_FOLDER ] || (git clone git@$CI_SERVER_HOST:$CI_PROJECT_NAMESPACE/$REPO_NAME.git $CODE_FOLDER &&
-  cd $CODE_FOLDER) && exit"
+  [ -e $CODE_FOLDER ] || git clone git@$CI_SERVER_HOST:$CI_PROJECT_NAMESPACE/$REPO_NAME.git $CODE_FOLDER && exit"
 ssh -i ~/.ssh/deploy_key -o StrictHostKeyChecking=no -p $SSH_PORT $SSH_USERNAME@$SSH_HOST "
-  cd $CODE_FOLDER;
-  git checkout $REPO_BRANCH;
+  cd $CODE_FOLDER;git checkout $REPO_BRANCH;
   git pull;
   echo \"$ENV\" > .env;
   make docker-stop postgres || echo \"Postgres not run\";
